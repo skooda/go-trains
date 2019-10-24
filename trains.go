0c3f7a02629d5d4824dbc4bbfbc3b0c9998ab43f
Pavel Škoda <p@v3l.cz>
2019-10-24 17:22:57 +0200

    Add message queue

--- trains.go
+++ trains.go
@@ -5,21 +5,28 @@ import (
 	"sync"
 )
 
-func platform(s string, wg *sync.WaitGroup) {
-	fmt.Println("Train arrived to platform " + s)
+func platform(s string, wg *sync.WaitGroup, mq *chan string) {
+	fmt.Println(<-*mq + " arrived to platform " + s)
 	wg.Done()
 }
 
 func main() {
+	// Init message queue
+	messageQueue := make(chan string)
 
 	// Add waitgroup as counter for coroutines
 	var wg sync.WaitGroup
 	defer wg.Wait()
 
 	// Add some coroutines
-	go platform("one", &wg)
-	go platform("two", &wg)
-	go platform("three", &wg)
+	go platform("one", &wg, &messageQueue)
+	go platform("two", &wg, &messageQueue)
+	go platform("three", &wg, &messageQueue)
 	wg.Add(3) // ... and add them into counter
+
+	// Send some trains into queue
+	messageQueue <- "Train 1"
+	messageQueue <- "Train 2"
+	messageQueue <- "Train 3"
 
 }

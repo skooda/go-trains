9a72ecaa84143d76448af2731b64d5b9c59513c2
Pavel Škoda <p@v3l.cz>
2019-10-25 23:05:24 +0200

    Add tracks between stations

--- trains.go
+++ trains.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"sync"
+	"time"
 )
 
 type train struct {
@@ -14,39 +14,58 @@ type station struct {
 	name string
 }
 
-func platform(station station, id int, wg *sync.WaitGroup, mq chan train) {
-	for true {
-		train := <-mq
+func platform(station station, id int, arrival chan train, departure chan train) {
+	for true { // Persistent platform
+		train := <-arrival
 		fmt.Println(station.name + ": Train " + strconv.Itoa(train.id) + " arrived to platform " + strconv.Itoa(id))
-		wg.Done()
+		time.Sleep(time.Second) // train waiting in the station
+		fmt.Println(station.name + ": Train " + strconv.Itoa(train.id) + " leaving platform " + strconv.Itoa(id))
+		departure <- train
 	}
 }
 
-func main() {
-	// Init message queue
-	messageQueue := make(chan train)
+// track represents an rail between two stations
+func track(arrival chan train, departure chan train) {
+	train := <-arrival
 
-	// Add waitgroup as counter for coroutines
-	var wg sync.WaitGroup
-	defer wg.Wait()
+	fmt.Println("Train " + strconv.Itoa(train.id) + " is on the way")
+	time.Sleep(5 * time.Second) // simulate train journey
+	fmt.Println("Train " + strconv.Itoa(train.id) + " is near the station")
+
+
+	departure <- train // train leaves the track and arrives some station
+}
+
+func main() {
+	maxTrainsOnTrack := 10
 
 	// Add some stations
 	prague := station{name: "Prague"}
 	paris := station{name: "Paris"}
 
+	// Init message queue
+	pragueIn := make(chan train, maxTrainsOnTrack - 1)
+	pragueOut := make(chan train, maxTrainsOnTrack - 1)
+	parisIn := make(chan train, maxTrainsOnTrack - 1)
+	parisOut := make(chan train, maxTrainsOnTrack - 1)
+
 	// Add some platforms as coroutines
-	go platform(prague, 1, &wg, messageQueue)
-	go platform(prague, 2, &wg, messageQueue)
-	go platform(paris, 1, &wg, messageQueue)
-	go platform(paris, 2, &wg, messageQueue)
-	go platform(paris, 3, &wg, messageQueue)
+	go platform(prague, 1, pragueIn, pragueOut)
+	go platform(prague, 2, pragueIn, pragueOut)
+	go platform(paris, 1, parisIn, parisOut)
+	go platform(paris, 2, parisIn, parisOut)
+	go platform(paris, 3, parisIn, parisOut)
+
+	go track(pragueOut, parisIn)
+	go track(parisOut, pragueIn)
 
 	// Send some trains into queue
-	trainCount := 10
-	wg.Add(trainCount) // ... and add them into counter
+	trainCount := 5
 
-	for i := 0; i < trainCount; i++ {
-		messageQueue <- train{id: i}
+	for i := 0; i < trainCount; i+=2 {
+		pragueIn <- train{id: i} // Some trains is starting in prague
+		parisIn <- train{id: i+1} // .. and some in paris
 	}
 
+	time.Sleep(60 * time.Second) // Disabled wg and just run the simulation for one minute for now
 }
